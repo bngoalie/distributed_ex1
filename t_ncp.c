6115c62ce7c2a9b587025da5213a4cdd095cd087
Ethan Bennis <etb513@gmail.com>
2014-09-21 04:06:30 -0400

    Now have functional TCP programs. Still need timing

--- t_ncp.c
+++ t_ncp.c
@@ -7,90 +7,80 @@
 	
 int main(int argc, char *argv[])
 {
-	struct 	sockaddr_in 	host;
-	struct 	hostent		h_ent, *p_h_ent;
-	char	host_name[80];
-	
-	FILE	*file_src;	/* File source */
-	char	*file_dst;	/* File destination */
-	char	*target;	/* Target machine */
-	
-	int	s;		/* Socket */
-	int	ret;
-	int	nread;
-	int	mess_len;
-	char	mess_buf[MAX_PACKET_SIZE];
-	char	*neto_mess_ptr = &mess_buf[sizeof(mess_len)];
+	struct 	    sockaddr_in host;
+	struct 	    hostent		h_ent, *p_h_ent;
+	int 	    s;		    /* Socket */
+	int         ret;
+    int	        bytes = 0;
+    int         done = 0;
+    int         name_len;
+	char	    file_buf[MAX_PACKET_SIZE];
+    char        *test;
+    char        *file_dst = &file_buf[sizeof(name_len)];
+	char	    *target;	/* Target machine string */
+	FILE	    *file_src;	/* File source handler */
 
 	/* Open file source and get destination from arguments: */
-	if ((file_src = fopen(argv[1], "r")) == NULL)
-	{
+	if ((file_src = fopen(argv[1], "r")) == NULL) {
 		perror("fopen");
 		exit(0);
 	}
 
-	file_dst = strtok(argv[2], "@");
+	test = strtok(argv[2], "@");
 	target = strtok(NULL, "\0");
+
+    printf("Destination filename: %s\n", test);
 
 	/* Open TCP socket: */
 	s = socket(AF_INET, SOCK_STREAM, 0);
-	if (s<0) 
-	{
-		perror("t_ncp: socket error");
+	if (s<0) {
+		perror("t_ncp: socket error\n");
 		exit(1);
 	}
-
 	host.sin_family = AF_INET; 	/* Set internet address family */
 	host.sin_port	= htons(PORT); 	/* Set port */
 
 	/* Get target machine info from name: */
 	p_h_ent = gethostbyname(target);
-	if (p_h_ent == NULL)
-	{
+	if (p_h_ent == NULL) {
 		printf("t_ncp: gethostbyname error.\n");
 		exit(1);
 	}
-
 	memcpy(&h_ent, p_h_ent, sizeof(h_ent));
 	memcpy(&host.sin_addr, h_ent.h_addr_list[0], sizeof(host.sin_addr));
 
 	/* Open TCP connection: */
 	ret = connect(s, (struct sockaddr *)&host, sizeof(host));
-	if (ret < 0)
-	{
-		perror("t_ncp: could not connect to server");
+	if (ret < 0) {
+		perror("t_ncp: could not connect to server\n");
 		exit(1);
 	}
 
-	/* Send file: */
-	for (;;)
+    /* Send filename (and size) in first packet: */
+    name_len = strlen(test) + sizeof(name_len);
+    memcpy(file_buf, &name_len, sizeof(name_len));
+    memcpy(file_dst, test, strlen(test));
+    send(s, file_buf, name_len, 0);
+
+	/* Send file chunks: */
+	while(!done)
 	{
-		nread = fread(mess_buf, 1, MAX_PACKET_SIZE, file_src);
-		if (nread > 0)
-			/* SEND PACKET */
-			printf("SEND PACKET HERE\n");
-			ret = send(s, mess_buf, nread, 0);
-			if (ret != nread)
-			{
-				perror("t_ncp: error in writing chunk");
-				exit(0);
-			}
-		else if (nread < MAX_PACKET_SIZE)
-		{
-			if (feof(file_src))
-			{ 
-				printf("Finished writing!\n");
-				break;
-			}
-			else
-			{
-				perror("t_ncp: error in reading file\n");
-				exit(0);
-			}
+        printf("running loop");
+        /* Read chunk from file */
+		bytes = fread(file_buf, 1, MAX_PACKET_SIZE, file_src); 
+		if (bytes > 0) { 
+            /* Send chunk */
+			send(s, file_buf, bytes, 0);
+            /* Close if EOF */
+            if (feof(file_src))
+                done = 1;
+		} else { /* Else 0 bytes read (file divides into packet sizei) */
+            send(s, file_buf, 0, 0);    /* Send empty closing packet*/
+            done = 1;
 		}
 	}
 
-	/* Close file: */
-	fclose(file_src);
+    /* Close and return */
+    fclose(file_src);
 	return 0;
 }
--- t_rcv.c
+++ t_rcv.c
@@ -2,43 +2,47 @@
 
 int main()
 {
-	struct	    sockaddr_in name;
-	int	        s;
 	fd_set	    mask;
-	int	        recv_s[10];
-	int	        valid[10];
 	fd_set	    dummy_mask, temp_mask;
-	int	        i,j,num;
-	int	        mess_len;
-	int 	    neto_len;
+    struct      sockaddr_in name;
+    int         mess_len;
+    int         name_len;
+	int	        s;
+	int	        num;
 	char	    mess_buf[MAX_PACKET_SIZE];
-	long	    on=1;
-    TcpPacket   *packet;
-    struct      sockaddr_in from_addr;
-    socklen_t   from_len;
-    int         bytes;
+    long	    on=1;
+    int         bytes = 0;
+    int         conn = 0;
     int         done = 0;
     FILE        *fw = NULL;
-
-	/* Create TCP socket: */
+    
+	/* Create TCP socket */
 	s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s<0) {
 		perror("t_rcv: socket");
 		exit(1);
 	}
 
+    /* Configure socket */
 	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) < 0) {
 		perror("t_rcv: bind");
 		exit(1);
 	}
+    name.sin_family = AF_INET;
+    name.sin_addr.s_addr = INADDR_ANY;
+    name.sin_port = htons(PORT);
 
+    /* Bind and listen */
+    if ( bind( s, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
+        perror("t_rcv: bind");
+        exit(1);
+    }
 	if (listen(s, 4) < 0) {
 		perror("t_rcv: listen");
 		exit(1);
 	}
 
 	/* Set up multiplexed I/O masks: */
-	i = 0;
 	FD_ZERO(&mask);
 	FD_ZERO(&dummy_mask);
 	FD_SET(s,&mask);
@@ -49,39 +53,39 @@ int main()
 		num = select(FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
 		if (num > 0) {
 			if (FD_ISSET(s, &temp_mask)) {
-                /* Get data from ethernet interface */
-                from_len    = sizeof(from_addr);
-                bytes       = recvfrom( s, mess_buf, sizeof(mess_buf), 0,
-                              (struct sockaddr *)&from_addr, &from_len );
-
-                /* Form packet */
-                packet = malloc(sizeof(TcpPacket));                 
-                if (!packet) {
-                    printf("Malloc failed for new tranfer queue node.\n");
-                    exit(0);
-                }
-                memcpy((char *)packet, mess_buf, bytes);
-
-                /* Handle according to type */
-                if (packet->type == (PACKET_TYPE) 0) {
-                    /* Filename packet - open file for writing */
-                    if((fw = fopen(packet->payload, "w")) == NULL) {
-                        perror("t_rcv: fopen");
-                        exit(0);
+                /* If no connection, establish and get filename */
+                if (!conn) {
+                    s = accept (s, 0, 0);
+                    FD_SET(s, &mask);
+                    conn = 1;
+                    printf("Connection established\n");
+                    if (recv(s, &mess_len,sizeof(mess_len), 0) > 0) {
+                        name_len = mess_len - sizeof(mess_len);
+                        bytes = recv(s, mess_buf, name_len, 0 );
+                        mess_buf[name_len] = '\0';
+                        printf("Opening file %s for reading...\n", &mess_buf[0]);
+                        /* Filename packet - open file for writing */
+                        if((fw = fopen(mess_buf, "w")) == NULL) {
+                            perror("t_rcv: fopen");
+                            exit(0);
+                        }
+                    } else {
+                        perror("t_rcv: zero bytes returned from TCP stream");
+                        exit(1);
                     }
-                } else if (packet->type == (PACKET_TYPE) 1) {
-                    /* Data packet - write to file*/
-                    fwrite(packet->payload, 1, bytes - sizeof(PACKET_TYPE), fw);
-                } else if (packet->type == (PACKET_TYPE) 2) {
-                    /* End packet - write to file and close file */
-                    fwrite(packet->payload, 1, bytes - sizeof(PACKET_TYPE), fw);
-                    fclose(fw);
-                    done = 1;
-                }
-
-                /* We're done with the packet, free it! */
-                free(packet);
-			}
-		}
-	}	
+                } else {  /* If connection is already established, get data */
+                    bytes = recv(s, mess_buf, MAX_PACKET_SIZE, 0);
+                    if (bytes > 0) {
+                        fwrite(&mess_buf[0], 1, bytes, fw);
+                    }
+                    if (bytes < MAX_PACKET_SIZE || feof(fw))
+                        done = 1;
+                }   
+            }
+        }
+    }
+    
+    /* Close and return */
+    fclose(fw);
+    return 0;	
 }
